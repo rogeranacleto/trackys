8b36e148b1d3b8c7116d69de02db46401e458c76
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-08 08:39:17 -0300

    feat: created pages navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Login } from "./components/Login"
+import { createBrowserRouter } from "react-router";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
+import { Home } from "./components/Home";
+const route = createBrowserRouter([
+  {
+    element: <Home/>,
+    path: "/"
+  },
+  {
+    element: <Login/>,
+    path:"/login"
+  },
+  {
+    element: <Signup/>,
+    path:"/signup"
+  }
+])
 
-function App() {
-  return(
-    <div>
-      <Login/>
-    </div>
-
-  )
-}
-
-export default App
+export { route }
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,7 @@
+export function Home(){
+    return(
+        <div>
+            <h1 className="text-white">Homepage</h1>
+        </div>
+    )
+}
