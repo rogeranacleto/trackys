b08fc41a03fe509c0a2b1cf36824a16bc1ac9e8f
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-10 18:30:50 -0300

    shaduicn dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server:{ 
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server:{
     open: true
   }
-},
-
-)
+})
