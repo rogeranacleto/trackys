6f203eb0b685102f1bcb7571109dccb78f3b67c8
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-11 07:16:19 -0300

    new layout dependencies

--- src/components/HeaderHome/index.tsx
+++ src/components/HeaderHome/index.tsx
@@ -3,6 +3,7 @@ import logo from "../../assets/logo-trackys-2.png"
 export function HeaderHome(){
 
     return(
+        
             <header className="flex justify-center p-5 absolute top-0 left-0 w-full bg-black/0.2 backdrop-blur-md z-50">
                 <div className="flex items-center max-w-6xl w-full justify-between mt-4">
                     <div className="flex items-center">
@@ -16,7 +17,7 @@ export function HeaderHome(){
                             <button className="text-[#a6ff00af] text-lg cursor-pointer hover:text-[#a6ff00] hover:scale-110 duration-700 ease-in-out ">Entrar</button>
                         </Link>
                         <Link to={"/signup"}>
-                            <button className=" cursor-pointer text-[#a6ff00] text-lg border border-solid border-[#a6ff00 pl-3.5 pr-3.5 pt-1 pb-1 hover:bg-[#a6ff00] hover:text-[#000] duration-700 ease-in-out">Crie sua conta</button>
+                            <button className="cursor-pointer text-[#a6ff00] text-lg border border-solid border-[#a6ff00] pl-3.5 pr-3.5 pt-1 pb-1 hover:bg-[#a6ff00] hover:text-[#000] duration-700 ease-in-out">Crie sua conta</button>
                         </Link>
                     </div>
                 </div>
