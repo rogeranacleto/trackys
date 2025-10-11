f0d8e3ceac3363a46be06161446af534ea5e8881
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-11 07:16:50 -0300

    change layout login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
-import { MdOutlineMail } from "react-icons/md";
-import { RiLockPasswordLine } from "react-icons/ri";
 import { Link } from "react-router";
+import { LoginForm } from "@/components/login-form"
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
@@ -15,21 +15,10 @@ useEffect(() => {
 
     return(
         <div>
-            <div data-aos="zoom-in" data-aos-duration="1000" className="w-screen h-screen bg-[#000] flex flex-col justify-center items-center p-5">
-                <section className="flex flex-col max-w-2xl w-full relative">
-                    <h1 className="text-white text-3xl text-center mb-3.5">Bem vindo à <span className="text-[#a6ff00]">Trackys</span></h1>
-                    <p className="text-gray-100/60 mb-4.5 text-center">Insira seu e-mail e senha, para começar a usar o sistema.</p>
-                    <label className="text-white mb-2">Email</label>
-                    <MdOutlineMail className="absolute text-[#a6ff00af] text-2xl bottom-80 left-5"/>
-                    <input type="email" placeholder="email@exemplo.com" className="border border-solid border-[#a6ff00af] text-gray-100/60 focus:outline-none focus:shadow-lg focus:ring-2 focus:ring-[#a6ff00] transition-shadow duration-300 pt-2.5 pb-2.5 pl-15 mb-10"/>
-                    <label className="text-white mb-2">Senha</label>
-                    <RiLockPasswordLine className="absolute text-[#a6ff00af] text-2xl bottom-50.5 left-5"/>
-                    <input type="password" placeholder="Insira sua senha" className="border border-solid border-[#a6ff00af] text-gray-100/60 focus:outline-none focus:shadow-lg focus:ring-2 focus:ring-[#a6ff00] transition-shadow duration-300 pt-2.5 pb-2.5 pl-15 mb-10 w-full"/>
-                    <button className="text-[#000] bg-[#a6ff00af] border border-solid border-[#a6ff00] cursor-pointer hover:bg-[#a6ff00] duration-700 ease-in-out p-2.5">Entrar</button>
-                        <Link to={"/signup"}>
-                            <p className="text-[#a6ff00] font-bold text-center mt-20">Não tem uma conta? Cadastre-se</p>
-                        </Link>
-                </section>
+            <div data-aos="zoom-in" data-aos-duration="1000" className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+                <div className="w-full max-w-sm">
+                    <LoginForm/>
+                </div>
             </div>
         </div>
     )
