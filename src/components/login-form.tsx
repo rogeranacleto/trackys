2c7d99f06d55d59c674c4868174fb3480c72d8d9
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-13 23:05:05 -0300

    feat: user login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/services/firebaseConnection"
+import { useState, type FormEvent } from "react"
+import { useNavigate } from "react-router"
 import {
   Card,
   CardContent,
@@ -16,10 +20,22 @@ import {
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 
-export function LoginForm({
-  className,
-  ...props
-}: React.ComponentProps<"div">) {
+export function LoginForm({className, ...props}: React.ComponentProps<"div">) {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const navigate = useNavigate();
+
+async function loginUser(e: FormEvent){
+  e.preventDefault();
+  try{
+    await signInWithEmailAndPassword(auth, email, password)
+    alert ("logado com sucesso")
+    navigate("/admin/dashboard")
+  }catch(error){
+    alert(error);
+  }
+}
+
   return (
     <div className={cn("flex flex-col gap-6 dark", className)} {...props}>
       <Card>
@@ -30,7 +46,7 @@ export function LoginForm({
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={loginUser}>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -39,13 +55,14 @@ export function LoginForm({
                   type="email"
                   placeholder="email@exemplo.com"
                   required
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </Field>
               <Field>
                 <div className="flex items-center">
                   <FieldLabel htmlFor="password">Senha</FieldLabel>
                 </div>
-                <Input id="password" type="password" required />
+                <Input id="password" type="password" required onChange={(e) => setPassword(e.target.value)}/>
               </Field>
               <Field>
                 <Button type="submit" className="cursor-pointer bg-[#dedede88] hover:bg-[#dedede] duration-400">Entrar</Button>
