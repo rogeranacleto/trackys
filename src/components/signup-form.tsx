4b1f560f12e89dc5b602ce92aee335d467c3f06d
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-13 22:51:51 -0300

    feat: create user

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button"
+import { auth } from "@/services/firebaseConnection"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
 import { Link } from "react-router"
+import { useState, type FormEvent } from "react"
+import { useNavigate } from "react-router"
 import {
   Card,
   CardContent,
@@ -16,6 +20,35 @@ import {
 import { Input } from "@/components/ui/input"
 
 export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+const navigate = useNavigate();
+
+async function createUser(e: FormEvent){
+  e.preventDefault();
+  if(password !== confirmPassword){
+    alert("As senhas devem ser iguais!")
+    return;
+  }
+  try{
+    const userCredential = createUserWithEmailAndPassword(auth, email, password)
+    const nameUser = (await userCredential).user
+    await updateProfile(nameUser, {
+      displayName: name
+    })
+    alert("cadastrado com sucesso")
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    navigate("/login")
+  }catch(error){
+    alert(error)
+  }
+}
+
   return (
     <Card className={("flex flex-col gap-6 dark")}{...props}>
       <CardHeader>
@@ -29,7 +62,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
           <FieldGroup>
             <Field>
               <FieldLabel htmlFor="name">Nome Completo</FieldLabel>
-              <Input id="name" type="text" placeholder="" required />
+              <Input id="name" type="text" placeholder="" required onChange={(e) => setName(e.target.value)}/>
             </Field>
             <Field>
               <FieldLabel htmlFor="email">Email</FieldLabel>
@@ -38,6 +71,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
                 type="email"
                 placeholder="email@exemplo.com"
                 required
+                onChange={(e) => setEmail(e.target.value)}
               />
               <FieldDescription>
                 Usaremos isso para entrar em contato com você. Não compartilharemos seu e-mail com mais ninguém.
@@ -45,7 +79,7 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
             </Field>
             <Field>
               <FieldLabel htmlFor="password">Senha</FieldLabel>
-              <Input id="password" type="password" required />
+              <Input id="password" type="password" required onChange={(e) => setPassword(e.target.value)}/>
               <FieldDescription>
                 Deve ter pelo menos 6 caracteres.
               </FieldDescription>
@@ -54,12 +88,12 @@ export function SignupForm({ ...props }: React.ComponentProps<typeof Card>) {
               <FieldLabel htmlFor="confirm-password">
                 Confirmar Senha
               </FieldLabel>
-              <Input id="confirm-password" type="password" required />
+              <Input id="confirm-password" type="password" required onChange={(e) => setConfirmPassword(e.target.value)}/>
               <FieldDescription>Por favor confirme sua senha.</FieldDescription>
             </Field>
             <FieldGroup>
               <Field>
-                <Button type="submit" className="cursor-pointer bg-[#dedede88] hover:bg-[#dedede] duration-400">Criar conta</Button>
+                <Button type="submit" className="cursor-pointer bg-[#dedede88] hover:bg-[#dedede] duration-400" onClick={createUser}>Criar conta</Button>
                 <FieldDescription className="px-6 text-center">
                   Ja tem uma conta? <Link to={"/login"} className="hover:text-[#D0D0D0] duration-400">Entrar</Link>
                 </FieldDescription>
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,10 +1,14 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import { SiteHeader } from "@/components/site-header";
 import { Outlet } from "react-router";
 
 export function Layout(){
     return(
         <div>
-            <AppSidebar/>
+            <div className="flex">
+                <AppSidebar/>
+                <SiteHeader/>
+            </div>
             <main>
                 <Outlet/>
             </main>
