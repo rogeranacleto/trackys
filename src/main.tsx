d2bbc913b8987052f57d3c6432885b6899bacce3
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-14 22:46:01 -0300

    toaster configuration the main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,44 @@
+document.documentElement.classList.add('dark')
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { route } from './App'
 import { RouterProvider } from 'react-router'
+import { Toaster } from 'react-hot-toast';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+  <Toaster
+      position="bottom-right"
+      reverseOrder={false}
+      gutter={10}
+      containerStyle={{
+        padding: '10',
+      }}
+      toasterId="default"
+      toastOptions={{
+        className: '', 
+        duration: 5000,
+        removeDelay: 1000,
+        style: {
+          border: '1px solid #F3F4F633',
+          background: '#000000',      
+          color: '#fff',          
+          borderRadius: '10px',   
+          boxShadow: '0 4px 12px #000000', 
+          padding: '16px 25px',   
+          fontFamily: 'sans-serif',
+        },
+        success: {
+          duration: 5000,
+          iconTheme: {
+            primary: 'green',
+            secondary: 'white',
+          },
+        },
+      }}
+    />
     <RouterProvider router={route}></RouterProvider>
   </StrictMode>,
 )
