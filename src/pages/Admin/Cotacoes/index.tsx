ce6556643d8c4cb14e8be2e0bb0ada45a992f93c
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-21 23:40:40 -0300

    style: transportation fee page structure

--- src/pages/Admin/Cotacoes/index.tsx
+++ src/pages/Admin/Cotacoes/index.tsx
@@ -1,8 +1,10 @@
+import { SiteHeader } from "@/components/site-header"
 export function Cotacoes(){
     return(
-        <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] p-4">
+        <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] pt-3 pb-3 pr-3 pl-10">
             <div className="max-w-screen w-full min-h-screen h-full bg-[#000] rounded-2xl">
-                <h1 className="text-[#fff]">Página do dashboard</h1>
+                <SiteHeader/>
+                <h1>Seção de registro de cotações</h1>
             </div>
         </div>
     )
