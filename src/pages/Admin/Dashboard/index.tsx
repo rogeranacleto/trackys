16ca62646e027a4355b52cd0de25be634f1490b7
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-27 21:11:39 -0300

    style: change padding dashboard

--- src/pages/Admin/Dashboard/index.tsx
+++ src/pages/Admin/Dashboard/index.tsx
@@ -6,7 +6,7 @@ import data from "../../../app/dashboard/data.json"
 
 export function Dashboard(){
     return(
-        <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] pt-3 pb-3 pr-3 pl-10">
+        <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] p-4">
             <div className="max-w-screen w-full min-h-screen h-full bg-[#000] rounded-2xl">
                 <SiteHeader/>
                 <SectionCards/>
