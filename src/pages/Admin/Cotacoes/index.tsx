8d154280bcc7c0fded512d9694594e2a759973e2
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-10-29 23:01:38 -0300

    style: section to add quote

--- src/pages/Admin/Cotacoes/index.tsx
+++ src/pages/Admin/Cotacoes/index.tsx
@@ -1,17 +1,31 @@
 import { SiteHeader } from "@/components/site-header"
 import { IoSearch } from "react-icons/io5";
 import { SectionCards } from "@/components/section-cards";
+import { BsSendPlus } from "react-icons/bs";
 export function Cotacoes(){
     return(
         <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] pt-3 pb-3 pr-3 pl-10 box-border">
             <div className="max-w-screen w-full min-h-screen h-full bg-[#000] rounded-2xl">
                 <SiteHeader/>
                 <SectionCards/>
-                <div className="m-5 relative">
+                <div className="mr-8 ml-8 relative">
                     <IoSearch className="absolute top-2 right-3.5 text-[#333333] text-2xl"/>
                     <input type="text" className="w-full border border-solid border-[#333333] p-2 rounded-sm focus:outline-0 focus:border-[#555353]" placeholder="Procurar cotações"/>
                 </div>
-                <div className="m-5">
+                <div className="mr-8 ml-8 mb-8 mt-8">
+                    <div className="w-full border border-solid border-[#333333] h-50 rounded-sm flex justify-between items-center p-15">
+                        <div>
+                            <h1 className="text-[#fff] text-3xl">Adicionar Nova Cotação</h1>
+                            <p className="text-[#a39d9d] mt-2.5">Adicione cotações para escolher uma transportadora para seus envios.</p>
+                        </div>
+                        <div>
+                            <button className="border border-solid border-[#333333] p-3.5 rounded-full flex items-center justify-center cursor-pointer">
+                                <BsSendPlus className="text-3xl"/>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="mr-8 ml-8">
                     <div className="w-full overflow-x-auto border border-solid border-[#333333] rounded-sm">
                         <table className="w-full text-left border-collapse">
                             <thead className="border-b">
