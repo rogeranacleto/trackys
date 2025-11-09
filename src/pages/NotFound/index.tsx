96586340b2961106a102f4d33e60f9d27bcacaf5
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-08 21:53:39 -0300

    style: change home buton

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -13,7 +13,7 @@ export function NotFound(){
                     </h1>
                     <p className="text-white text-4xl mb-8">Ops, página não encontrada!</p>
                     <Link to={"/"}>
-                        <Button className="max-w-lg w-full m-auto bg-[#dedede88] cursor pointer text-[#000] hover:bg-[#dedede] p-6 duration-400 ease-in-out cursor-pointer text-lg">Página inicial</Button>
+                        <Button className="max-w-lg w-full m-auto bg-[#dedede88] cursor pointer text-[#000] hover:bg-[#dedede] pl-3.5 pr-3.5 pt-1 pb-1 duration-400 ease-in-out cursor-pointer text-lg">Página inicial</Button>
                     </Link>
                 </div>
             </div>
