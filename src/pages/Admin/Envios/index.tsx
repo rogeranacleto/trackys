9dab529acdda73383b3e06ccbbbfae3e9a5f56f2
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-15 23:32:19 -0300

    style: table in envios

--- src/pages/Admin/Envios/index.tsx
+++ src/pages/Admin/Envios/index.tsx
@@ -1,7 +1,14 @@
+import { SiteHeader } from "@/components/site-header";
+import { DataTable } from "@/components/data-table";
+import data from "../../../app/dashboard/data.json"
+
 export function Envios(){
     return(
-        <div>
-            <h1>Página sobre os envios</h1>
+        <div className="max-w-screen w-full min-h-screen h-full bg-[#171717] p-4 box-border">
+            <div className="max-w-screen w-full min-h-screen h-full bg-[#000] rounded-2xl">
+                <SiteHeader/>
+                <DataTable data={data}/>
+            </div>
         </div>
     )
 }
