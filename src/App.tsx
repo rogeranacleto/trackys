ac9fe84398417b99e033ea523d457f24784a0609
Roger Anacleto <rogerdasilvacode7@gmail.com>
2025-11-18 23:27:01 -0300

    style: change of route name

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,25 @@ import { NotFound } from "./pages/NotFound";
 import { Layout } from "./layout";
 import { Cotacoes } from "./pages/Admin/Cotacoes";
 import { Dashboard } from "./pages/Admin/Dashboard";
-import { Entregas } from "./pages/Admin/Entregas";
+import { Ranking } from "./pages/Admin/Ranking";
 import { Envios } from "./pages/Admin/Envios";
 import { Indicadores } from "./pages/Admin/Indicadores";
 import { Ocorrencias } from "./pages/Admin/Ocorrencias";
 import { PrivateRoute } from "./components/PrivateRoute";
 
 const route = createBrowserRouter([
-  {
-    element: <Home/>,
-    path: "/"
-  },
-  {
-    element: <Login/>,
-    path:"/login"
-  },
-  {
-    element: <Signup/>,
-    path:"/signup"
-  },
+{
+  element: <Home/>,
+  path: "/"
+},
+{
+  element: <Login/>,
+  path:"/login"
+},
+{
+  element: <Signup/>,
+  path:"/signup"
+},
   {
     element: <PrivateRoute/>,
     children:[
@@ -44,8 +44,8 @@ const route = createBrowserRouter([
             path: "/admin/envios"
           },
           {
-            element: <Entregas/>,
-            path: "/admin/entregas"
+            element: <Ranking/>,
+            path: "/admin/ranking"
           },
           {
             element: <Indicadores/>,
